5a9f0f0b6456e2470a0598f32d5e3a279716b942
Mateusz Turcza <mturcza@przyneta.(none)>
2012-07-24 21:26:25 +0200

    zaczatek implementacji schemy modulu scholar

---
+++ modules/scholar/scholar.module.php
@@ -0,0 +1,9 @@
+<?php
+
+function scholar_form(&$node, $form_state)
+{
+    // Nie dopuszczaj do bezposredniej modyfikacji wezlow
+    // aktualizowanych automatycznie przez modul scholar.
+    // Podobnie z wykorzystawymi eventami.
+    
+)
