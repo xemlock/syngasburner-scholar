0cb816bd8292f4ab0f8ebdf8ae89ccc136579bad
xemlock <xemlock@gmail.com>
2012-07-25 13:27:29 +0200

    schema modulu scholar gotowa

--- modules/scholar/scholar.install.php
+++ modules/scholar/scholar.install.php
@@ -2,7 +2,7 @@
 
 function scholar_schema()
 {
-    $schema['scholar_people'] = array(
+    $schema['scholar_people'] = array( // {{{
         'description' => 'osoby - autorzy prac, prowadzący wykłady etc.',
         'fields' => array(
             'id' => array(
@@ -19,37 +19,18 @@ function scholar_schema()
                 'length'    => 255,
                 'not null'  => true,
             ),
-            'title' => array(
-                'description' => 'tytuł: dr, mgr, mgr inż. etc',
-                'type'      => 'varchar',
-                'length'    => 24,
-            ),
-            'create_time' => array(
-                'type'      => 'datetime',
-                'not null'  => true,
-            ),
-            'created_by' => array(
-                'description' => 'id użytkownika, który utworzył rekord',
-                'type'      => 'int',
-                'not null'  => true,
-            ),
             'photo' => array(
                 'description' => 'zdjęcie osoby',
                 'type'      => 'varchar',
                 'length'    => 255,
             ),
-            'node_id' => array( // wezel musi byc dla kazdego jezyka
-                'description' => 'id wezła, w którym umieszczana będzie treść',
-                'type'      => 'int',
-                'not null'  => true,
-            ),
         ),
         'primary key'  => array('id'),
         'mysql_suffix' => 'CHARACTER SET utf8 COLLATE utf8_polish_ci',
-    );
+    ); // }}}
 
-    $schema['scholar_categories'] = array(
-        'description' => 'typy czasopism, załączników, etc.',
+    $schema['scholar_categories'] = array( // {{{
+        'description' => 'typy czasopism, załączników, wykładów',
         'fields' => array(
             'id' => array(
                 'type'      => 'serial',
@@ -58,263 +39,140 @@ function scholar_schema()
             'table_name' => array(
                 'description' => 'do rekordów jakiej tabeli można zastostować te kategorie, nazwa tabeli bez prefiksu scholar_',
                 'type'      => 'varchar',
-                'length'    => 255,
+                'length'    => 32,
+                'not null'  => true,
+            ),
+            'subtype' => array(
+                'description' => 'podtyp w tabeli, może być pusty jeżeli tabela tego nie wymaga',
+                'type'      => 'varchar',
+                'length'    => 32,
             ),
             'name' => array(
                 'description' => 'angielska nazwa kategorii, jej tłumaczenie poprzez moduł Translate',
                 'type'      => 'varchar',
+                'length'    => 255,
                 'not null'  => true,
+            ),
+            'color' => array(
+                'description' => 'kolor do oznaczenia obiektów danej kategorii np. w kalendarzu',
+                'type'      => 'varchar',
+                'length'    => 7,
             ),
         ),
         'primary key'  => array('id'),
         'unique keys'  => array(
-            'name' => array('table_name', 'name'),
+            'name' => array('table_name', 'subtype', 'name'),
         ),
         'mysql_suffix' => 'CHARACTER SET utf8 COLLATE utf8_polish_ci',
-    );
+    ); // }}}
 
-    $schema['scholar_journals'] = array(
-        'description' => 'czasopisma i monografie',
+    $schema['scholar_objects'] = array( // {{{
+        'description' => 'Generyczna tabela na obiekty: kontenery na publikacje: czasopisma, monografie lub wykłady: konferencje, seminaria, oraz ich elementy',
         'fields' => array(
             'id' => array(
                 'type'      => 'serial',
                 'not null'  => true,
+            ),
+            'parent_id' => array(
+                'type'      => 'int', // references scholar_objects (id)
+            ),
+            'subtype' => array(
+                // predefiniowane: 
+                //   journal (czasopismo, książka, monografia, proceedings), 
+                //   conference (jak journal ale z dokładną datą początku),
+                //   article (element składowy journal),
+                //   lecture (element składowy conference),
+                //   block (generyczny kontener używany w CV, istotny tylko tytuł),
+                //   resume (element bloku w CV)
+                'description' => 'predefiniowany typ, raz ustalony nie podlega zmianie',
+                'type'      => 'varchar', 
+                'length'    => 32,
+                'not null'  => true,
+            ), 
+            'category' => array(
+                'description' => 'kategoria w obrębie podtypu, REFERENCES scholar_categories (id)', // np. podtypem conference jest konferencja, warsztaty lub seminarium
+                'type'      => 'int',
+                'not null'  => true,
+            ),
+            'start_date'     => array(
+                'type'      => 'datetime',
+            ),
+            'end_date' => array(
+                'type'      => 'datetime',
             ),
             'title' => array(
                 'type'      => 'varchar',
                 'length'    => 255,
                 'not null'  => true,
             ),
-            'category' => array(
-                'type'      => 'int',
-                'not null'  => true,
-                // references scholar_categories (id)
-            ),
-            'url' => array(
+            'details' => array(
+                'description' => 'article: dodatkowa specyfikacja uzupełniająca bibliografię',
                 'type'      => 'varchar',
                 'length'    => 255,
             ),
-            'node_id' => array(
+            'url' => array(
+                'description' => 'zewnętrzny URL strony czasopisma lub konferencji',
+                'type'      => 'varchar',
+                'length'    => 255,
+            ),
+            'gallery_id' => array(
+                'description' => 'id galerii - każdy rekord może mieć powiązaną galerię, unikalną dla siebie',
                 'type'      => 'int',
-                'not null'  => true,
             ),
         ),
         'primary key' => array('id'),
-        'mysql_suffix' => 'CHARACTER SET utf8 COLLATE utf8_polish_ci',
-    );
-
-    $schema['scholar_articles'] = array(
-        'description' => 'artykuły, prace, wykłady, prezentacje',
-        'fields' => array(
-            'id' => array(
-                'type'      => 'serial',
-                'not null'  => true,
-            ),
-            'title' => array(
-                'type'      => 'varchar',
-                'length'    => 255,
-                'not null'  => true,
-            ),
-            'year' => array(
-                'type'      => 'int',
-                'not null'  => true,
-            ),
-            'journal' => array(
-                'type'      => 'varchar',
-                'length'    => 255,
-                // references scholar_journals (id)
-            ),
-            'details' => array(
-                'type'      => 'varchar',
-                'length'    => 255,
-            ),
+        'indexes' => array(
+            'parent_id'        => array('parent_id'),
+            'subtype_category' => array('subtype', 'category'),
         ),
-        'primary key'  => array('id'),
         'mysql_suffix' => 'CHARACTER SET utf8 COLLATE utf8_polish_ci',
-    );
+    ); // }}}
 
-    $schema['scholar_authors'] = array(
+    $schema['scholar_authors'] = array( // {{{
         'description' => 'autorzy artykułów',
         'fields' => array(
             'person_id' => array(
+                'description' => 'REFERENCES scholar_people (id)',
                 'type'      => 'int',
                 'not null'  => true,
-                // references scholar_people (id)
             ),
-            'article_id' => array(
+            'object_id' => array(
+                'description' => 'REFERENCES scholar_objects (id)',
                 'type'      => 'int',
                 'not null'  => true,
-                // references scholar_articles (id)
+            ),
+            'author_order' => array(
+                'description' => 'kolejność autorów artykułu',
+                'type'      => 'int',
+                'not null'  => true,
+                'default'   => 0,
             ),
         ),
-        'primary key'  => array('person_id', 'article_id'),
-    );
+        'primary key'  => array('person_id', 'object_id'),
+    ); // }}}
 
-    // to musi byc wielojezykowe, mamy node ids
-    $schema['scholar_events'] = array(
-        'description' => 'Konferencje, seminaria, warsztaty etc.',
+    $schema['scholar_files'] = array( // {{{
+        'description' => 'pliki podpięte do wpisów',
         'fields' => array(
-            'id' => array(
-                'type'      => 'serial',
+            'file_id' => array(
+                'description' => 'REFERENCES files (fid)',
+                'type'      => 'int',
                 'not null'  => true,
             ),
-            'start_date' => array(
-                'type'      => 'datetime',
+            'object_id' => array(
+                'description' => 'REFERENCES scholar_objects (id)',
+                'type'      => 'int',
                 'not null'  => true,
             ),
-            'end_date'  => array(
-                'type'      => 'datetime',
+            'category_id' => array(
+                'description' => 'REFERENCES scholar_categories (id)',
+                'type'      => 'int',
+                'not null'  => true,
             ),
         ),
-    );
+        'primary key' => array('file_id'),
+    ); // }}}
 
-/*
-  $schema['gallery_data'] = array( // {{{
-    'description' => 'Gallery metadata.',
-    'fields' => array(
-      'gallery_id' => array( // references gallery (id)
-        'description' => 'Id galerii.',
-        'type' => 'int',
-        'unsigned' => true,
-        'not null' => true,
-      ),
-      'lang' => array(
-        'description' => 'Jezyk w jakim napisane zostaly metadane.',
-        'type' => 'char',
-        'length' => 2,
-        'not null' => true,
-      ),
-      'title' => array(
-        'description' => 'Tytul galerii.',
-        'type' => 'varchar',
-        'length' => 255,
-        'not null' => true,
-      ),
-      'description' => array(
-        'description' => 'Opis galerii.',
-        'type' => 'text',
-        'not null' => false,
-      ),
-    ),
-    'primary key' => array('gallery_id', 'lang'),
-  ); // }}}
-
-  $schema['gallery_node'] = array( // {{{
-    'description' => 'References between galleries and nodes.', 
-    'fields' => array(
-      'node_id' => array(
-        'description' => 'Node identifier.',
-        'type' => 'int', // references node (nid)
-        'unsigned' => true,
-        'not null' => true,        
-      ),
-      'gallery_id' => array(
-        'description' => 'Gallery identifier.',
-        'type' => 'int', // references gallery (id)
-        'unsigned' => true,
-        'not null' => true,        
-      ),
-      'layout' => array(
-        'description' => 'Gallery layout type.',
-        'type' => 'varchar',
-        'length' => 16,
-        'not null' => true,
-        'default' => 'vertical',
-      ),
-    ),
-    'primary key' => array('gallery_id', 'node_id'),
-  ); // }}}
- 
-  $schema['image'] = array( // {{{
-    'description' => 'Image table.',
-    'fields' => array(
-      'id' => array(
-        'description' => 'Image identifier.',
-        'type' => 'serial',
-        'unsigned' => true,
-        'not null' => true,
-      ),
-      'gallery_id' => array(
-        'description' => 'Id galerii, do ktorej nalezy obraz.',
-        'type' => 'int', // references gallery (id)
-        'unsigned' => true,
-        'not null' => false,
-      ),
-      'mimetype' => array(
-        'description' => 'Typ MIME.',
-        'type' => 'varchar',
-        'length' => 255,
-        'not null' => true,
-      ),
-      'filename' => array(
-        'description' => 'Nazwa pliku.',
-        'type' => 'varchar',
-        'length' => 255,
-        'not null' => true,
-      ),
-      'filesize' => array(
-        'description' => 'Rozmiar obrazu.',
-        'type' => 'varchar',
-        'length' => 255,
-        'not null' => true,
-      ),
-      'width' => array(
-        'description' => 'Szerokosc obrazu (px).',
-        'type' => 'int',
-        'unsigned' => true,
-        'not null' => true,
-      ),
-      'height' => array(
-        'description' => 'Wysokosc obrazu (px).',
-        'type' => 'int',
-        'unsigned' => true,
-        'not null' => true,
-      ),
-      'created' => array(
-        'description' => 'Data dodania do bazy.',
-        'type' => 'datetime',
-        'not null' => true,
-      ),
-      'weight' => array(
-        'description' => 'Kolejnosc sortowania w galerii.',
-        'type' => 'int',
-        'not null' => true,
-        'default' => 0,
-      ),
-    ),
-    'primary key' => array('id'),
-  ); // }}}
-
-  $schema['image_data'] = array( // {{{
-    'description' => 'Metadane obrazu.',
-    'fields' => array(
-      'image_id' => array( // references image (id)
-        'description' => 'Id obrazu.',
-        'type' => 'int',
-        'unsigned' => true,
-        'not null' => true,
-      ),
-      'lang' => array(
-        'description' => 'Jezyk w jakim napisane zostaly metadane.',
-        'type' => 'char',
-        'length' => 2,
-        'not null' => true,
-      ),
-      'title' => array(
-        'description' => 'Tytul obrazu.',
-        'type' => 'varchar',
-        'length' => 255,
-        'not null' => true,
-      ),
-      'description' => array(
-        'description' => 'Opis obrazu.',
-        'type' => 'text',
-        'not null' => false,
-      ),
-    ),
-    'primary key' => array('image_id', 'lang'),
-  ); // }}}
-*/
   return $schema;
 }
 
--- modules/scholar/scholar.module.php
+++ modules/scholar/scholar.module.php
@@ -1,9 +1,17 @@
 <?php
 
-function scholar_form(&$node, $form_state)
+function scholar_form_alter(&$form, &$form_state, $form_id)
 {
     // Nie dopuszczaj do bezposredniej modyfikacji wezlow
     // aktualizowanych automatycznie przez modul scholar.
     // Podobnie z wykorzystawymi eventami.
-    
-)
+    $node = $form['#node'];
+    echo '<pre>', __FUNCTION__, ': ', $form_id, '</pre>';
+}
+
+function scholar_nodeapi($node, $op)
+{
+    if ($op == 'load') {
+//        echo '<pre>', $op, ': ', print_r($node, 1), '</pre>';
+    }
+}
