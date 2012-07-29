240928f921658eb558d41f966d818aabee9a1e44
xemlock <xemlock@gmail.com>
2012-07-29 14:58:29 +0200

    przywrocenie scholar.module.php

---
+++ modules/scholar/scholar.module.php
@@ -0,0 +1,115 @@
+<?php
+
+function scholar_perm() {
+  return array('administer scholar', 'manage scholar contents');
+}
+
+function scholar_form_alter(&$form, &$form_state, $form_id)
+{
+    // Nie dopuszczaj do bezposredniej modyfikacji wezlow
+    // aktualizowanych automatycznie przez modul scholar.
+    // Podobnie z wykorzystawymi eventami.
+    echo '<code>', $form_id, '</code>';
+    if ('page_node_form' == $form_id  && $form['#node']) {
+        $query = db_query("SELECT * FROM {scholar_nodes} WHERE node_id = %d", $form['#node']->nid);
+        $row = db_fetch_array($query);
+        if ($row) {
+            switch ($row['table_name']) {
+                case 'people':
+                    $url = 'scholar/people/edit/' . $row['object_id'];
+                    break;
+
+                default:
+                    $url = null;
+                    break;
+            }
+            echo '<h1 style="color:red">Direct modification of scholar-referenced nodes is not allowed!</h1>';
+            if ($url) {
+                echo '<p>You can edit scholar object <a href="' . url($url) . '">here</a>.</p>';
+            }
+            // exit;
+        }
+    }
+}
+
+function scholar_nodeapi($node, $op)
+{
+    if ($op == 'load') {
+        echo '<pre>', $op, ': ', print_r($node, 1), '</pre>';
+    }
+}
+
+function scholar_menu()
+{
+    $items = array();
+
+    $items['scholar'] = array(
+        'title'             => t('Scholar'),
+        'access arguments'  => array('use scholar'),
+        'page callback'     => 'scholar_index',
+    );
+
+    $items['scholar/people'] = array(
+        'title'             => t('People'),
+        'access arguments'  => array('use scholar'),
+        'page callback'     => 'scholar_people_list',
+        'parent'            => 'scholar',
+        'file'              => 'scholar.people.php',
+    );
+    $items['scholar/people/list'] = array(
+        'type'              => MENU_DEFAULT_LOCAL_TASK,
+        'title'             => t('List'),
+        'weight'            => -10, // na poczatku listy
+    );
+    $items['scholar/people/add'] = array(
+        'type'              => MENU_LOCAL_TASK,
+        'title'             => t('Add person'),
+        'access arguments'  => array('use scholar'),
+        'page callback'     => 'scholar_render_form',
+        'page arguments'    => array('scholar_people_form'),
+        'parent'            => 'scholar/people',
+        'file'              => 'scholar.people.php',
+    );
+    $items['scholar/people/edit/%'] = array(
+        'type'              => MENU_CALLBACK,
+        'title'             => t('Edit person'),
+        'access arguments'  => array('use scholar'),
+        'page callback'     => 'scholar_render_form',
+        'page arguments'    => array('scholar_people_form', 3),
+        'parent'            => 'scholar/people',
+        'file'              => 'scholar.people.php',
+    );
+    $items['scholar/people/delete/%'] = array(
+        'type'              => MENU_CALLBACK,
+        'title'             => t('Delete person'),
+        'access arguments'  => array('use scholar'),
+        'page callback'     => 'scholar_render_form',
+        'page arguments'    => array('scholar_people_delete_form', 3),
+        'parent'            => 'scholar/people',
+        'file'              => 'scholar.people.php',
+    );
+    return $items;
+}
+
+function scholar_index()
+{
+    return '<pre>' . print_r(func_get_args(), 1) . '</pre>';
+}
+
+function scholar_render($html)
+{
+    if (isset($_REQUEST['modal'])) {
+        echo '<html><head><title>Scholar modal</title></head><body>' . $html . '</body></html>';
+        exit;
+    }
+    return $html;
+}
+
+function scholar_render_form()
+{
+    $args = func_get_args();
+    $html = call_user_func_array('drupal_get_form', $args);
+    return scholar_render($html);
+}
+
+require_once dirname(__FILE__) . '/scholar.node.php';
