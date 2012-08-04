d14c3e1b7603c0ad8d5093fdf2017036fe3411f1
xemlock <xemlock@gmail.com>
2012-08-05 01:52:39 +0200

    uruchomienie tabledrag w attachment_managerze

--- modules/scholar/scholar.js
+++ modules/scholar/scholar.js
@@ -3,25 +3,29 @@ $(function() {
 $('.scholar-attachment-manager').each(function() {
     var prefix = $(this).attr('data-name');
 
-    var html = '<table id="temp-table"><thead><tr><th>File</th>';
-    html += '<th>Label (Polish)</th>' + '<th>Label (English)</th>';    
+    var html = '<table id="temp-table" class="sticky-enabled"><thead><tr><th>File</th><th>Size</th>';
+    html += '<th>Label (Polish)</th>' + '<th>Label (English)</th>';
     html += '</tr></thead>';
     html += '<tbody>';
     
-    html += '<tr class="odd"><td>Plik 1</td><td><input type=text/></td><td><input type=text/></td></tr>';
-    html += '<tr class="even"><td>Plik 2</td><td><input type=text/></td><td><input type=text/></td></tr>';
-    html += '<tr class="odd"><td>Plik 3</td><td><input type=text/></td><td><input type=text/></td></tr>';    
+    html += '<tr class="draggable odd"><td>Plik 1</td><td>22 KB</td><td><img style="display:inline" title="Polish" alt="" src="/ventures/i/flags/pl.png"> <input type=text/></td><td><img style="display:inline" title="English" alt="" src="/ventures/i/flags/en.png"> <input type=text/><input class="weight" type="hidden" value=0 /></td></tr>';
+    html += '<tr class="draggable even"><td>Plik 2</td><td>333 KB</td><td><img style="display:inline" title="Polish" alt="" src="/ventures/i/flags/pl.png"> <input type=text/></td><td><img style="display:inline" title="English" alt="" src="/ventures/i/flags/en.png"> <input type=text/><input class="weight" type="hidden" value=0 /></td></tr>';
+    html += '<tr class="draggable odd"><td>Plik 3</td><td>4.44 MB</td><td><img style="display:inline" title="Polish" alt="" src="/ventures/i/flags/pl.png"> <input type=text/></td><td><img style="display:inline" title="English" alt="" src="/ventures/i/flags/en.png"> <input type=text/><input class="weight" type="hidden" value=0 /></td></tr>';
 
     html += '</tbody>';
 
     $(this).html(html);
 
-    new Drupal.tableDrag($('#temp-table')[0], {
-        target: 'temp-table',
-        source: 'temp-table',
+
+    var td = new Drupal.tableDrag($('#temp-table')[0], {weight: [{
+        target: 'weight',
+        source: 'weight',
         relationship: 'sibling',
-        action: 'order'
-    });
+        action: 'order',
+        hidden: false,
+        limit: 0
+    }] });
+    console.log(td);
 });
 
 });
