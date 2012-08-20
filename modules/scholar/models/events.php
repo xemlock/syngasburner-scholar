a9b3c7bc69d70368dfc3f96860e0b20156eb67e6
xemlock <xemlock@gmail.com>
2012-08-20 22:06:04 +0200

    ustawianie start_date jezeli nie zostala podana, usuwanie eventow

--- modules/scholar/models/events.php
+++ modules/scholar/models/events.php
@@ -1,10 +1,17 @@
 <?php
 
 /**
- * Pobiera z bazy danych wydarzenia powiązane z rekordem podanej tabeli.
+ * Pobiera z bazy danych wydarzenia powiązane z rekordem wybranej tabeli.
+ *
  * @param int $row_id
+ *     identyfikator rekordu
  * @param string $table_name
+ *     ciąg znaków identyfikujący tebelę, w której znajduje się rekord
+ *     o podanym identyfikatorze
  * @return array
+ *     tablica z powiązanymi wydarzeniami, której kluczami są kody języka
+ *     każdego z rekordów. Niepowtarzalność kodów języka jest gwarantowana
+ *     przez definicję tabeli przechowującej powiązania z rekordami wydarzeń.
  */
 function scholar_attachments_load_events($row_id, $table_name) // {{{
 {
@@ -27,10 +34,19 @@ function scholar_attachments_load_events($row_id, $table_name) // {{{
 } // }}}
 
 /**
+ * Zapisuje rekordy wydarzeń i wiąże je z rekordem wybranej tabeli.
+ *
+ * @param int $row_id
+ *     identyfikator rekordu
+ * @param string $table_name
+ *     ciąg znaków identyfikujący tebelę, w której znajduje się rekord
+ *     o podanym identyfikatorze
  * @param array $events
- *     tablica nowych wartości eventów
+ *     tablica z danymi rekordów wydarzeń, zawierająca na pierwszym poziomie
+ *     jako klucze kody języków, a jako wartości tablice z danymi pojedynczych
+ *     rekordów. Może być to wartość pochodząca z {@see scholar_events_form}.
  * @return int
- *     liczba zapisanych (utworzonych / zaktualizowanych) rekordów
+ *     liczba zapisanych (utworzonych lub zaktualizowanych) rekordów
  */
 function scholar_attachments_save_events($row_id, $table_name, $events) // {{{
 {
@@ -39,10 +55,7 @@ function scholar_attachments_save_events($row_id, $table_name, $events) // {{{
     if (module_exists('events')) {
         // zapisz dowiazane eventy, operuj tylko na wezlach w aktualnie
         // dostepnych jezykach
-        p($events);
         foreach (scholar_languages() as $language => $event_data) {
-
-
             // sprawdz czy istnieje relacja miedzy generykiem a eventem
             $event = false;
             $query = db_query("SELECT * FROM {scholar_events} WHERE generic_id = %d AND language = '%s'", $row_id, $language);
@@ -63,6 +76,20 @@ function scholar_attachments_save_events($row_id, $table_name, $events) // {{{
                 $event = events_new_event();
             }
 
+            // jezeli dane pochodza z formularza scholar_events_form, wartosci
+            // dat zapisane sa pod kluczami bezposrednio w tablicy $events,
+            // a nie dla kazdego eventu z osobna. Jezeli nie podano wymaganej
+            // daty poczatku zdarzenia uzyj czasu epoki Uniksa.
+            if (empty($event_data['start_date'])) {
+                $event_data['start_date'] = isset($events['start_date'])
+                    ? $events['start_date']
+                    : date('Y-m-d H:i:s', 0);
+            }
+
+            if (empty($event_data['end_date']) && isset($events['end_date'])) {
+                $event_data['end_date'] = $events['end_date'];
+            }
+
             // skopiuj dane do eventu...
             foreach ($event_data as $key => $value) {
                 // ... pilnujac, zeby nie zmienic klucza glownego!
@@ -80,14 +107,16 @@ function scholar_attachments_save_events($row_id, $table_name, $events) // {{{
 
             // zapisz event
             if (events_save_event($event)) {
-                // usun wczesniejsze powiazania
+                // usun wczesniejsze powiazania...
                 db_query("DELETE FROM {scholar_events} WHERE (generic_id = %d AND language = '%s') OR (event_id = %d)",
                     $row_id, $language, $event->id
                 );
-                // dodaj nowe
+
+                // ... i dodaj nowe
                 db_query("INSERT INTO {scholar_events} (generic_id, event_id, language, body) VALUES (%d, %d, '%s', '%s')",
                     $row_id, $event->id, $language, $body
                 );
+
                 ++$count;
             }
         }
@@ -96,8 +125,27 @@ function scholar_attachments_save_events($row_id, $table_name, $events) // {{{
     return $count;
 } // }}}
 
-function scholar_delete_events($row_id, $table_name)
+/**
+ * Usuwa rekordy wydarzeń powiązane z rekordem wybranej tabeli.
+ *
+ * @param int $row_id
+ *     identyfikator rekordu
+ * @param string $table_name
+ *     ciąg znaków identyfikujący tebelę, w której znajduje się rekord
+ *     o podanym identyfikatorze
+ */
+function scholar_delete_events($row_id, $table_name) // {{{
 {
-}
+    if (module_exists('events')) {
+        $query = db_query("SELECT * FROM {scholar_events} WHERE generic_id = %d", $row_id);
+
+        while ($row = db_fetch_array($query)) {
+            $event = events_load_event($row['event_id']);
+            events_delete_event(&$event);
+        }
+
+        db_query("DELETE FROM {scholar_events} WHERE generic_id = %d", $row_id);
+    }
+} // }}}
 
 // vim: fdm=marker
