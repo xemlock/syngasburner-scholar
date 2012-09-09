deadb27afb1557110670d4da66f3f1dc958ef4d7
xemlock <xemlock@gmail.com>
2012-09-09 21:45:19 +0200

    dokumentacja do include/render

--- modules/scholar/include/render.php
+++ modules/scholar/include/render.php
@@ -6,9 +6,12 @@
  * wartość. Jeżeli podano nową, zostanie ona ustawiona, przy czym zwrócona
  * zostanie poprzednia wartość.
  *
- * @param bool $enabled OPTIONAL        true żeby włączyć renderowanie,
- *                                      false aby wyłączyć
+ * @param bool $enabled
+ *     opcjonalny, true żeby włączyć renderowanie, false aby wyłączyć
  * @return bool
+ *     jeżeli podano nową wartość $enabled funkcja zwraca poprzednią.
+ *     Jeżeli funkcję wywołano bez argumentu zwrócona zostanie aktualna
+ *     wartość ustawienia.
  */
 function _scholar_rendering_enabled($enabled = null) // {{{
 {
@@ -25,8 +28,8 @@ function _scholar_rendering_enabled($enabled = null) // {{{
 } // }}}
 
 /**
- * Funkcja wywoływana po pomyślnym zapisie lub usunięciu rekordów
- * osób, kategorii i rekordów generycznych oraz przy usuwaniu / zmianie nazwy plików.
+ * Funkcja wywoływana po pomyślnym zapisie lub usunięciu rekordów osób,
+ * kategorii i rekordów generycznych oraz przy usuwaniu / zmianie nazwy plików.
  * Zmiana lub usunięcie wydarzeń i węzłów nie wpływa na rendering. 
  */
 function scholar_invalidate_rendering() // {{{
@@ -34,6 +37,15 @@ function scholar_invalidate_rendering() // {{{
     variable_set('scholar_last_change', date('Y-m-d H:i:s'));
 } // }}}
 
+/**
+ * Generuje kod BBCode prezentujący spis publikacji i wystąpień osoby
+ * o podanym identyfikatorze.
+ *
+ * @param scholar_view $view
+ * @param int $id
+ * @param object $node
+ * @return string
+ */
 function scholar_render_people_node($view, $id, $node) // {{{
 {
     $vars = scholar_report_person($id, $node->language);
@@ -47,6 +59,16 @@ function scholar_render_people_node($view, $id, $node) // {{{
         ->render('person.tpl');
 } // }}}
 
+/**
+ * Funkcja pomocnicza wywołująca funkcję odpowiedzialną za generowanie kodu
+ * BBCode dla rekordu generycznego o podanym identyfikatorze. Wywoływana
+ * funkcja dla danego podtypu musi mieć nazwę postaci:
+ * <code>scholar_render_generics_{subtype}_node</code>
+ *
+ * @param scholar_view $view
+ * @param int $id
+ * @param object $node
+ */
 function scholar_render_generics_node($view, $id, $node) // {{{
 {
     $generic = scholar_load_record('generics', $id);
@@ -58,6 +80,15 @@ function scholar_render_generics_node($view, $id, $node) // {{{
     }
 } // }}}
 
+/**
+ * Generuje kod BBCode prezentujący spis prezentacji w obrębie podanej
+ * konferencji.
+ *
+ * @param scholar_view $view
+ * @param object $conference
+ * @param object $node
+ * @return string
+ */
 function scholar_render_generics_conference_node($view, $conference, $node) // {{{
 {
     $vars = scholar_report_conference($conference->id, $node->language);
@@ -68,6 +99,16 @@ function scholar_render_generics_conference_node($view, $conference, $node) // {
         ->render('conference.tpl');
 } // }}}
 
+/**
+ * Funkcja pomocnicza wywołująca funkcję odpowiedzialną za generowanie kodu
+ * BBCode dla strony specjalnej o podanym identyfikatorze. Wywoływana funkcja
+ * dla danego podtypu strony musi mieć nazwę postaci:
+ * <code>scholar_render_pages_{subtype}_node</code>
+ *
+ * @param scholar_view $view
+ * @param int $id
+ * @param object $node
+ */
 function scholar_render_pages_node($view, $id, $node) // {{{
 {
     $page = scholar_load_record('pages', $id);
@@ -81,6 +122,14 @@ function scholar_render_pages_node($view, $id, $node) // {{{
     }
 } // }}}
 
+/**
+ * Generuje kod BBCode prezentujący spis wszystkich publikacji zapisanych
+ * w bazie danych.
+ *
+ * @param scholar_view $view
+ * @param object $node
+ * @return string
+ */
 function scholar_render_pages_publications_node($view, $node) // {{{
 {
     $vars = scholar_report_publications($node->language);
@@ -94,6 +143,14 @@ function scholar_render_pages_publications_node($view, $node) // {{{
         ->render('publications.tpl');
 } // }}}
 
+/**
+ * Generuje kod BBCode spisu wszystkich prezentacji na konferencjach zapisanych
+ * w bazie danych.
+ *
+ * @param scholar_view $view
+ * @param object $node
+ * @return string
+ */
 function scholar_render_pages_conferences_node($view, $node) // {{{
 {
     $vars = scholar_report_conferences($node->language);
@@ -108,8 +165,11 @@ function scholar_render_pages_conferences_node($view, $node) // {{{
 } // }}}
 
 /**
- * Wywołuje funkcję odpowiedzialną za listowanie rekordów należących do
- * kategorii o podanym identyfikatorze.
+ * Funkcja pomocnicza wywołująca funkcję odpowiedzialną za listowanie rekordów
+ * należących do kategorii o podanym identyfikatorze. Wywoływana funkcja
+ * dla danej tabeli i opcjonalnie danego podtypu musi mieć nazwę postaci
+ * (w nawiasach kwadratowych ujęta została opcjonalna część nazwy):
+ * <code>scholar_render_categories_{table_name}[_{subtype}]_node</code>
  *
  * @param scholar_view $view
  * @param int $id
