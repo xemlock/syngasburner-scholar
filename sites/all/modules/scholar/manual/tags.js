c92b1addf26ffbcf11f175389234786a91355c16
xemlock <xemlock@gmail.com>
2012-11-21 14:45:31 +0100

    dokumentacja

--- sites/all/modules/scholar/manual/tags.js
+++ sites/all/modules/scholar/manual/tags.js
@@ -1,61 +1,61 @@
 {
     "Formatowanie tekstu": {
-        "b": {
-            "syn": "[b]{tekst}[/b]",
-            "desc": "Pogrubia podany tekst",
-            "ex": "[b]Tekst pogrubiony[/b]",
-            "res": "<b>Tekst pogrubiony</b>"
+        "[b]": {
+            "syn"  : "[b]{tekst}[/b]",
+            "desc" : "Pogrubia podany tekst",
+            "ex"   : "[b]Tekst pogrubiony[/b]",
+            "res"  : "<b>Tekst pogrubiony</b>"
         },
-        "i": {
+        "[i]": {
             "syn": "[i]{tekst}[/i]",
             "desc": "Pochyla podany tekst",
             "ex": "[i]Tekst pochylony[/i]",
             "res": "<i>Tekst pochylony</i>"
         },
-        "s": {
+        "[s]": {
             "syn": "[s]{tekst}[/s]",
             "desc": "Przekreśla podany tekst",
             "ex": "[s]Tekst przekreślony[/s]",
             "res": "<s>Tekst przekreślony</s>"
         },
-        "u": {
+        "[u]": {
             "syn": "[u]{tekst}[/u]",
             "desc": "Podkreśla podany tekst",
             "ex": "[u]Tekst podkreślony[/u]",
             "res": "<u>Tekst podkreślony</u>"
         },
-        "sub": {
+        "[sub]": {
             "syn": "[sub]{tekst}[/sub]",
             "desc": "Indeks dolny",
             "ex": "CO[sub]2[/sub]",
             "res": "CO<sub>2</sub>"
         },
-        "sup": {
+        "[sup]": {
             "syn": "[sup]{tekst}[/sup]",
             "desc": "Indeks górny",
             "ex": "200 mg/Nm[sup]3[/sup]",
             "res": "200 mg/Nm<sup>3</sup>"
         },
-        "color": {
+        "[color]": {
             "syn": "[color]{tekst}[/color]",
             "desc": "Kolor tekstu",
-            "notes": "Podany kolor może być nazwą koloru zdefiniowaną w CSS Level 3 (<a href=\"http://www.w3.org/TR/css3-color\">http://www.w3.org/TR/css3-color</a>), albo kolorem w postaci szesnastkowej <code>#rrggbb</code> lub <code>#rgb</code>.",
+            "note": "Podany kolor może być nazwą koloru zdefiniowaną w CSS Level 3 (<a href=\"http://www.w3.org/TR/css3-color\">http://www.w3.org/TR/css3-color</a>), albo kolorem w postaci szesnastkowej <code>#rrggbb</code> lub <code>#rgb</code>.",
             "ex": "[color=\"#0000FF\"]Niebieski tekst[/color]",
             "res": "<span style=\"color:#0000FF\">Niebieski tekst</span>"
         },
-        "size": {
+        "[size]": {
             "syn": "[size=\"{rozmiar}\"]{tekst}[/size]",
             "desc": "Rozmiar tekstu",
-            "notes": "Nowy rozmiar tekstu musi być podany w procentach.",
+            "note": "Nowy rozmiar tekstu musi być podany w procentach.",
             "ex": "[size=\"80%\"]1[/size][size=\"120%\"]2[/size][size=\"160%\"]3[/size][size=\"200%\"]4[/size]",
             "res": "<span style=\"font-size:80%\">1</span><span style=\"font-size:120%\">2</span><span style=\"font-size:160%\">3</span><span style=\"font-size:200%\">4</span>"
         }
     },
     "Odnośniki, obrazy i multimedia": {
-        "img": {
+        "[img]": {
             "syn": "[img]{adres_obrazu}[/img]",
             "desc": "Obraz",
-            "notes": "Podany adres musi być poprawnym względnym lub absolutnym (rozpoczynającym się od http(s)://) adresem URL, w przeciwnym razie wynik będzie pusty. Jeżeli podano adres względny, zostanie on automatycznie przekształcony w bezwzględny poprzez dodanie do jego początku adresu bezwzględnego bieżącej instalacji Drupala.",
+            "note": "Podany adres musi być poprawnym względnym lub absolutnym (rozpoczynającym się od http(s)://) adresem URL, w przeciwnym razie wynik będzie pusty. Jeżeli podano adres względny, zostanie on automatycznie przekształcony w bezwzględny poprzez dodanie do jego początku adresu bezwzględnego bieżącej instalacji Drupala.",
             "attrs": {
                 "width":  {"optional":true, "desc": "szerokość obrazu"},
                 "height": {"optional":true, "desc": "wysokość obrazu"},
@@ -65,13 +65,13 @@
             "ex": "[img]etc13.png[/img]",
             "res": "<img src=\"etc13.png\" alt=\"\" />"
         },
-        "url": {
+        "[url]": {
             "syn": [
                 "[url]{adres}[/url]",
                 "[url=\"{adres}\"]{tytuł_odnośnika}[/url]"
             ],
             "desc": "Odnośnik",
-            "notes": "Podany adres musi być poprawnym względnym lub bezwzględnym (rozpoczynającym się od http(s)://) adresem URL. Jeżeli podano adres względny, zostanie on automatycznie przekształcony w bezwzględny poprzez dodanie do jego początku adresu bezwzględnego bieżącej instalacji Drupala.",
+            "note": "Podany adres musi być poprawnym względnym lub bezwzględnym (rozpoczynającym się od http(s)://) adresem URL. Jeżeli podano adres względny, zostanie on automatycznie przekształcony w bezwzględny poprzez dodanie do jego początku adresu bezwzględnego bieżącej instalacji Drupala.",
             "ex": [
                 "[url]http://syngasburner.eu[/url]",
                 "[url=\"http://syngasburner.eu\"]Strona projektu[/url]"
@@ -81,11 +81,12 @@
                 "<a href=\"http://syngasburner.eu\">Strona projektu</a>"
             ]
         },
-        "youtube": {
-            "syn": "[youtube]{identyfikator_filmu}[/youtube]",
-            "desc": "Film z serwisu YouTube",
-            "notes": "Aby zmienić wymiary filmu trzeba podać atrybuty określające szerokość i wysokość filmu, w przeciwym razie zostaną użyte wymiary domyślne 640x360 px. Poprawny identyfikator filmu składa się z 11 znaków: cyfr, liter małych i wielkich, myślników oraz podkreśleń.",
-             "attrs": {
+        "[youtube]": {
+            "syn"  : "[youtube]{identyfikator_filmu}[/youtube]",
+            "desc" : "Film z serwisu YouTube",
+            "block": true,
+            "note" : "Aby zmienić wymiary filmu trzeba podać atrybuty określające szerokość i wysokość filmu, w przeciwym razie zostaną użyte wymiary domyślne 640x360 px. Poprawny identyfikator filmu składa się z 11 znaków: cyfr, liter małych i wielkich, myślników oraz podkreśleń.",
+            "attrs": {
                 "width":  {
                     "optional": true,
                     "desc": "szerokość filmu"
@@ -95,27 +96,30 @@
                     "desc": "wysokość filmu"
                 }
             },
-            "ex": "[youtube]Jie3noHnvG0[/youtube]",
-            "res": "<img src=\"youtube.png\" alt=\"\" />"
+            "ex"   : "[youtube]Jie3noHnvG0[/youtube]",
+            "res"  : "<img src=\"youtube.png\" alt=\"\" />"
         }
     },
     "Listy": {
-        "*": {
-            "syn" : "[*]{tekst}",
-            "desc": "Element listy",
-            "notes": "Treść elementu listy kończy się albo znakiem nowej linii, albo znacznikiem kolejnego elementu, albo znacznikiem zamykającym listę."
+        "[*]": {
+            "syn"  : "[*]{tekst}",
+            "block": true,
+            "desc" : "Element listy",
+            "note" : "Treść elementu listy kończy się albo znakiem nowej linii, albo znacznikiem kolejnego elementu, albo znacznikiem zamykającym listę."
         },
-        "li": {
-            "syn" : "[li]{tekst}[/li]",
-            "desc": "Element listy"
+        "[li]": {
+            "syn"  : "[li]{tekst}[/li]",
+            "block": true,
+            "desc" : "Element listy"
         },
-        "list": {
-            "syn" : [
+        "[list]": {
+            "syn"  : [
                 "[list]\n{elementy_listy}\n[/list]",
                 "[list={typ_listy}]\n{elementy_listy}\n[/list]"
             ],
-            "desc": "Lista wypunktowana lub numerowana",
-            "notes": "Jeżeli nie podano typu listy będzie ona wypunktowana. Typ listy numerowanej może przyjąć jedną z następujących wartości: 1 (numerowanie liczbami arabskimi), A (numerowanie wielkimi literami), a (numerowanie małymi literami), I (numerowanie liczbami rzymskimi), i (numerowanie małymi liczbami rzymskimi). Numerowanie listy rozpoczyna się od wartości 1. Aby rozpocząć numerację od innej wartości należy określić ją w atrybucie start.",
+            "block": true,
+            "desc" : "Lista wypunktowana lub numerowana",
+            "note" : "Jeżeli nie podano typu listy będzie ona wypunktowana. Typ listy numerowanej może przyjąć jedną z następujących wartości: 1 (numerowanie liczbami arabskimi), A (numerowanie wielkimi literami), a (numerowanie małymi literami), I (numerowanie liczbami rzymskimi), i (numerowanie małymi liczbami rzymskimi). Numerowanie listy rozpoczyna się od wartości 1. Aby rozpocząć numerację od innej wartości należy określić ją w atrybucie start.",
             "attrs": {
                 "start": {
                     "optional": true,
@@ -123,13 +127,13 @@
                     "desc": "początek numeracji"
                 }
             },
-            "ex"  : [
+            "ex"   : [
                 "[list]\n[*] Punkt pierwszy\n[*] Punkt drugi\n[*] Punkt trzeci\n[/list]",
                 "[list=1]\n[*] Punkt pierwszy\n[*] Punkt drugi\n[*] Punkt trzeci\n[/list]",
                 "[list=A]\n[*] Punkt pierwszy\n[*] Punkt drugi\n[*] Punkt trzeci\n[/list]",
                 "[list=A start=4]\n[*] Punkt czwarty\n[*] Punkt piąty\n[*] Punkt szósty\n[/list]"
             ],
-            "res" : [
+            "res"  : [
                 "<ul><li>Punkt pierwszy</li><li>Punkt drugi</li><li>Punkt trzeci</li></ul>",
                 "<ol><li>Punkt pierwszy</li><li>Punkt drugi</li><li>Punkt trzeci</li></ol>",
                 "<ol type=\"A\"><li>Punkt pierwszy</li><li>Punkt drugi</li><li>Punkt trzeci</li></ol>",
@@ -138,41 +142,50 @@
         }
     },
     "Formatowanie zaawansowane": {
-        "code": {
+        "[code]": {
             "syn"  : "[code]{kod_źródłowy}[/code]",
-            "desc" : "Wyświetla podany tekst jako blok kodu źródłowego zachowując formatowanie i używajac czcionki o ustalonej szerokości znaków.",
+            "block": true,
+            "desc" : "Kod źródłowy",
+            "note" : "Wyświetla podany tekst jako blok kodu źródłowego zachowując formatowanie i używajac czcionki o ustalonej szerokości znaków.",
             "ex"   : "[code]#ifdef __cplusplus\ntypedef bool boolean;\n#else\ntypedef enum {false, true} boolean;\n#endif[/code]",
             "res"  : "<pre><code>#ifdef __cplusplus\ntypedef bool boolean;\n#else\ntypedef enum {false, true} boolean;\n#endif</code></pre>"
         },
-        "quote": {
+        "[quote]": {
             "syn"  : "[quote]{tekst}[/quote]",
-            "desc" : "Wyświetla podany tekstu jako cytat.",
+            "block": true,
+            "desc" : "Cytat",
+            "note" : "Wyświetla podany tekst jako wyróżniony blok cytatu.",
             "ex"   : "[quote]Artificial Intelligence is no match for natural stupidity.[/quote]",
             "res"  : "<div class=\"quote\"><blockquote><div>Artificial Intelligence is no match for natural stupidity.</div></blockquote></div>"
         },
-        "noparse" : {
+        "[noparse]" : {
             "syn"  : "[noparse]{tekst}[/noparse]",
-            "desc" : "Wyłącza interpretowanie znaczników BBCode.",
+            "desc" : "Wyłącza interpretowanie znaczników",
             "ex"   : "[noparse][url]http://www.fuw.edu.pl[/url][/noparse]",
             "res"  : "[url]http://www.fuw.edu.pl[/url]"
         },
-        "nonl2br": {
+        "[nonl2br]": {
             "syn"  : "[nonl2br]{tekst}[/nonl2br]",
-            "desc" : "Wszystkie przejścia do nowego wiersza między znacznikiem otwierającym i zamykającym zostają zignorowane. Nie dotyczy znaczników <code>[br]</code>."
+            "desc" : "Ignoruje znaki nowego wiersza",
+            "note" : "Wszystkie przejścia do nowego wiersza między znacznikiem otwierającym i zamykającym zostają zignorowane. Nie dotyczy znaczników <code>[br]</code>."
         },
-        "br": {
+        "[br]": {
             "syn"  : "[br]",
-            "desc" : "Przejście do nowego wiersza."
+            "desc" : "Przejście do nowej linii",
+            "note" : "Znacznik ten nie posiada znacznika zamykającego.",
+            "ex"   : "Ta część zdania znajduje się przed przejściem do nowej linii,[br]a ta po.",
+            "res"  : "Ta część zdania znajduje się przed przejściem do nowej linii,<br/>a ta po.",
         },
         "\\[": {
             "syn"  : "\\[",
             "desc" : "Lewy nawias kwadratowy",
-            "notes": "Równoznaczne z <code>[noparse][[/noparse]</code>."
+            "note" : "Równoznaczne z <code>[noparse][[/noparse]</code>."
         },
         "\\]": {
             "syn"  : "\\]",
             "desc" : "Prawy nawias kwadratowy",
-            "notes": "Równoznaczne z <code>[noparse]][/noparse]</code>."
+            "note" : "Równoznaczne z <code>[noparse]][/noparse]</code>."
         }
-    }
+    },
+    
 }
